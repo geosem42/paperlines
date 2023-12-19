f18a5db23a7bbd7c7c901270b56fdaded37f7f2b
George <geosem042@gmail.com>
2023-12-19 23:53:21 +0200

    Rewrote the store in Composition API

--- src/store.js
+++ src/store.js
@@ -1,60 +1,31 @@
 import { createPinia, defineStore } from "pinia";
+import { ref } from "vue";
 
 export const pinia = createPinia();
 const apiKey = import.meta.env.VITE_SEMANTIC_SCHOLAR_API_KEY;
 
-export const useStore = defineStore({
-  id: "main",
-  state: () => ({
-    papers: [],
-    offset: 0,
-    next: null,
-    total: 0,
-    paperDetails: null,
-    isLoading: false,
-    isFetching: false,
-    searchQuery: "",
-  }),
-  actions: {
-    setSearchQuery(query) {
-      this.searchQuery = query;
-    },
-    async fetchPapers() {
-      this.isFetching = true;
+export const useStore = defineStore("main", () => {
+  const papers = ref([]);
+  const offset = ref(0);
+  const next = ref(null);
+  const total = ref(0);
+  const paperDetails = ref(null);
+  const isLoading = ref(false);
+  const isFetching = ref(false);
+  const searchQuery = ref("");
 
-      try {
-        const response = await fetch(
-          `https://api.semanticscholar.org/graph/v1/paper/search?query=${encodeURIComponent(
-            this.searchQuery
-          )}&offset=${
-            this.offset
-          }&fields=paperId,title,abstract,referenceCount`,
-          {
-            headers: {
-              "x-api-key": apiKey,
-            },
-          }
-        );
+  const setSearchQuery = (query) => {
+    searchQuery.value = query;
+  };
 
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status}`);
-        }
+  const fetchPapers = async () => {
+    isFetching.value = true;
 
-        const data = await response.json();
-        this.papers = data.data;
-        this.offset = data.offset;
-        this.next = data.next;
-        this.total = data.total;
-      } catch (error) {
-        console.error("Fetching papers failed:", error);
-      } finally {
-        this.isFetching = false;
-      }
-    },
-    async fetchPaperDetails(paperId) {
-      this.isLoading = true;
+    try {
       const response = await fetch(
-        `https://api.semanticscholar.org/graph/v1/paper/${paperId}?fields=paperId,title,references`,
+        `https://api.semanticscholar.org/graph/v1/paper/search?query=${encodeURIComponent(
+          searchQuery.value
+        )}&offset=${offset.value}&fields=paperId,title,abstract,referenceCount`,
         {
           headers: {
             "x-api-key": apiKey,
@@ -67,8 +38,48 @@ export const useStore = defineStore({
       }
 
       const data = await response.json();
-      this.paperDetails = data;
-      this.isLoading = false;
-    },
-  },
+      papers.value = data.data;
+      offset.value = data.offset;
+      next.value = data.next;
+      total.value = data.total;
+    } catch (error) {
+      console.error("Fetching papers failed:", error);
+    } finally {
+      isFetching.value = false;
+    }
+  };
+
+  const fetchPaperDetails = async (paperId) => {
+    isLoading.value = true;
+    const response = await fetch(
+      `https://api.semanticscholar.org/graph/v1/paper/${paperId}?fields=paperId,title,references`,
+      {
+        headers: {
+          "x-api-key": apiKey,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    paperDetails.value = data;
+    isLoading.value = false;
+  };
+
+  return {
+    papers,
+    offset,
+    next,
+    total,
+    paperDetails,
+    isLoading,
+    isFetching,
+    searchQuery,
+    setSearchQuery,
+    fetchPapers,
+    fetchPaperDetails,
+  };
 });
