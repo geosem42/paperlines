faffc5f1afaafad90e0ef0c3088f6e55aaa39bef
George <geosem042@gmail.com>
2023-12-20 20:06:35 +0200

    Disable visualize button if referenceCount equals 0

--- src/components/PaperCard.vue
+++ src/components/PaperCard.vue
@@ -28,7 +28,7 @@ onMounted(() => {
   <div class="p-6 bg-white border border-gray-200 rounded-lg shadow">
     <div class="flex justify-between mb-1">
       <a @click.prevent="handleViewPaper" href="#" class="flex items-center">
-        <h5 class="text-2xl font-bold tracking-tight text-gray-700 hover:text-indigo-800">
+        <h5 class="text-2xl font-bold tracking-tight text-gray-700 hover:text-indigo-700">
           {{ paper.title }}
         </h5>
       </a>
@@ -45,9 +45,17 @@ onMounted(() => {
       No abstract available
     </p>
 
-    <router-link :to="`/papers/${paper.paperId}`" :disabled="paper.referenceCount === 0"
-      class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg disabled:bg-indigo-100 disabled:cursor-not-allowed">
-      Visualize
-    </router-link>
+    <div v-if="paper.referenceCount !== 0">
+      <router-link :to="`/papers/${paper.paperId}`"
+        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg">
+        Visualize
+      </router-link>
+    </div>
+    <div v-else>
+      <button disabled
+        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-indigo-100 cursor-not-allowed rounded-lg">
+        Visualize
+      </button>
+    </div>
   </div>
 </template>
